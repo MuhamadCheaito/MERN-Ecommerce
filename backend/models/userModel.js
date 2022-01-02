da4c7e88e962b63356289eac3d1fc5ebab7dc386
MuhamadCheaito <cheaitomuhamad.92@gmail.com>
2022-01-02 16:15:30 +0200

    implement pass-reset and pass-forgot

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -46,7 +46,7 @@ userSchema.pre("save", async function(next){
     if(!this.isModified("password")){
         next();
     }
-    this.password = await bycrypt.hash(this.password,10); 
+    this.password = await bcrypt.hash(this.password,10); 
 });
 
 // JWT TOKEN
@@ -67,7 +67,15 @@ userSchema.methods.comparePassword = async function (password) {
 userSchema.methods.getResetPasswordToken = function(){
     //Generating Token
     const resetToken = crypto.randomBytes(20).toString("hex");
-    
+
+    //Hashing and adding resetPasswordToken to userSchema
+    this.resetPasswordToken = crypto.createHash("sha256")
+                                    .update(resetToken)
+                                    .digest("hex");
+
+    this.resetPasswordExpire = Date.now() + 15 * 60 * 60 * 1000;
+
+    return resetToken;
 }
 
 module.exports = mongoose.model("User",userSchema);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const {registerUser, loginUser, logoutUser, getAllUsers} = require("../controllers/userController");
+const {registerUser, loginUser, logoutUser, getAllUsers, forgotPassword, resetPassword} = require("../controllers/userController");
 const { isAuthenticatedUser,authorizeRoles } = require("../middleware/auth");
+
+router.route("/users").get(isAuthenticatedUser,authorizeRoles("admin"), getAllUsers);
 
 router.route("/register").post(registerUser);
 
 router.route("/login").post(loginUser);
 
+router.route("/password/forgot").post(forgotPassword);
+
+router.route("/password/reset/:token").put(resetPassword);
+
 router.route("/logout").post(logoutUser);
 
-router.route("/users").get(isAuthenticatedUser,authorizeRoles("admin"), getAllUsers);
+
 
 module.exports = router;
