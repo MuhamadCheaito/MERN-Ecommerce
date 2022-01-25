ce691fe3b60904407f8b47df5154b57f8cddc36f
MuhamadCheaito <cheaitomuhamad.92@gmail.com>
2022-01-25 12:18:25 +0200

    fix quantity naming

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -38,7 +38,7 @@ const orderSchema = new mongoose.Schema({
                 type:Number,
                 required:true
             },
-            qunatity:{
+            quantity:{
                 type:Number,
                 required:true
             },
