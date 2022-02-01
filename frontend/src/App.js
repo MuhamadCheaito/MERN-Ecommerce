21921788572ec4c5a1ec63ed1e3cc822e01560b5
MuhamadCheaito <cheaitomuhamad.92@gmail.com>
2022-02-01 23:52:04 +0200

    work on front-end header and footer and homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/layout/Header/Header'
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import { useEffect } from 'react';
+import WebFont from 'webfontloader';
+import Footer from './components/layout/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    WebFont.load({
+      google:{
+        families:["Roboto","Droid Sans", "Chilanka"]
+      }
+    })
+  },[]);
+
+  return <Router>
+      <Header />
+      <Route path="/" component={Home} />
+      <Footer />
+  </Router>
 }
 
 export default App;
